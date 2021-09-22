0e1702c61392e95145831209d851f763c20c6add
Rei <33325105+Eilaluth@users.noreply.github.com>
2021-09-22 16:32:02 +0700

    AUTO BAN RAID

--- index.js
+++ index.js
@@ -21,6 +21,55 @@ client.aliases = new Discord.Collection(); //an collection for all your command-
 client.categories = fs.readdirSync("./commands/"); //categories
 client.cooldowns = new Discord.Collection(); //an collection for cooldown commands of each user
 
+// AUTO BAN SPAMMER
+const usersMap = new Map();
+const LIMIT = 4;
+const TIME = 10000;
+const DIFF = 2000;
+
+client.on('message', message => {
+  if(message.author.bot) return;
+  if(usersMap.has(message.author.id)) {
+    const userData = usersMap.get(message.author.id);
+    const { lastMessage, timer } = userData;
+    const difference = message.createdTimestamp - lastMessage.createdTimestamp;
+    let msgCount = userData.msgCount;
+    console.log(difference);
+    if(difference > DIFF) {
+      clearTimeout(timer);
+      console.log('Cleared timeout');
+      userData.msgCount = 1;
+      userData.lastMessage = message;
+      userData.timer = setTimeout(() => {
+        usersMap.delete(message.author.id);
+        console.log('Removed from RESET.');
+      }, TIME);
+      usersMap.set(message.author.id, userData);
+    }
+    else {
+      ++msgCount;
+      if(parseInt(msgCount) === LIMIT) {
+        message.guild.members.resolve(message.author.id).ban({ reason : 'Raid attempt!'})
+        message.channel.send({ embed: {'title': `Raid Detected!`,'description': `User <@${message.author.id}> got banned for raid attempt!`,'image': {'url': `https://raw.githubusercontent.com/Eilaluth/Ei/main/images/ban.png`}}})
+      } else {
+        userData.msgCount = msgCount;
+        usersMap.set(message.author.id, userData);
+      }
+    }
+  }
+  else {
+    let fn = setTimeout(() => {
+      usersMap.delete(message.author.id);
+      console.log('Removed from map.');
+    }, TIME);
+    usersMap.set(message.author.id, {
+      msgCount: 1,
+      lastMessage: message,
+      timer: fn
+    });
+  }
+});
+
 // anti badword and anti raid
 client.on("message", message => {
   if (BannedWords.some(word => message.toString().toLowerCase().trim().match(/\w+|\s+|[^\s\w]+/g) !== null && message.toString().toLowerCase().trim().match(/\w+|\s+|[^\s\w]+/g).includes(word))) { message.delete().catch(e => console.error("Couldn't delete message.")); message.channel.send({ embed: {'title': `Badword Detected!`,'description': `User <@${message.author.id}> got warn for using badword!`,'image': {'url': `https://raw.githubusercontent.com/Eilaluth/Kotegawa/main/img/kotegawa-angry-dua.gif`}}}) };
